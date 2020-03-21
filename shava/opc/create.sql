ab4c97c734e24af2b3603077c15369c7295b32f3
Eugene <shaluginjeka@gmail.com>
2020-03-22 02:51:45 +0300

    requirements changed

--- shava/opc/create.sql
+++ shava/opc/create.sql
@@ -1,10 +1,1 @@
-CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
-
-CREATE TABLE test(
-  time timestamptz,
-  metric text,
-  value numeric);
-
-SELECT * FROM create_hypertable('test', 'time');
-
-SELECT * FROM create_hypertable('api_measure', 'Time', migrate_data => true);
+SELECT create_hypertable('api_measure', 'Time', migrate_data => true, if_not_exists => TRUE);
