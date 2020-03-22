ec1ccc42a6d88d99a79881ace6dff1aea68459e1
grigoriishveps <grigorii.shveps@gmail.com>
2020-03-22 16:57:27 +0300

    Логирование+стиль+Это конец

--- frontend/src/limit_input.js
+++ frontend/src/limit_input.js
@@ -20,7 +20,7 @@ export default class LimitInput extends  React.Component{
         //const scale = this.props.scale;
         return (
             <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Recipient's username"
+                <input type="text" className="form-control" placeholder="Введите предел"
                        aria-label="Введите предел" aria-describedby="button-addon2" value={value} onChange={this.handleChange}/>
                     <div className="input-group-append">
                         <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={this.handleClick}>Button</button>
