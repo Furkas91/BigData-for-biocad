5f4b816f21d2812b52c70ac8f8b15eebf6acf6f6
Eugene <shaluginjeka@gmail.com>
2020-03-22 03:55:41 +0300

    models added

--- shava/api/models.py
+++ shava/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Measure(models.Model):
     class Meta:
         app_label = 'api'
@@ -12,4 +13,41 @@ class Measure(models.Model):
     Weight = models.FloatField(verbose_name='Weight')
     FlowRate = models.FloatField(verbose_name='FlowRate')
     CO2 = models.FloatField(verbose_name='CO2')
+
+
+class Ustavki(models.Model):
+    class Meta:
+        app_label = 'api'
+    Time = models.DateTimeField(verbose_name='Time', auto_now_add=True, db_index=True, primary_key=True)
+    Pressure = models.FloatField(verbose_name='Pressure')
+    Humidity = models.FloatField(verbose_name='Humidity')
+    TempIns = models.FloatField(verbose_name='TempIns')
+    TempWork = models.FloatField(verbose_name='TempWork')
+    pH = models.FloatField(verbose_name='pH')
+    Weight = models.FloatField(verbose_name='Weight')
+    FlowRate = models.FloatField(verbose_name='FlowRate')
+    CO2 = models.FloatField(verbose_name='CO2')
+
+
+class Error(models.Model):
+    class Meta:
+        app_label = 'api'
+    Time = models.DateTimeField(verbose_name='Time', auto_now_add=True, db_index=True, primary_key=True)
+    Pressure = models.FloatField(verbose_name='Pressure')
+    Humidity = models.FloatField(verbose_name='Humidity')
+    TempIns = models.FloatField(verbose_name='TempIns')
+    TempWork = models.FloatField(verbose_name='TempWork')
+    pH = models.FloatField(verbose_name='pH')
+    Weight = models.FloatField(verbose_name='Weight')
+    FlowRate = models.FloatField(verbose_name='FlowRate')
+    CO2 = models.FloatField(verbose_name='CO2')
+
+
+class Log(models.Model):
+    class Meta:
+        app_label = 'api'
+    Time = models.DateTimeField(verbose_name='Time', auto_now_add=True, db_index=True, primary_key=True)
+    Type = models.CharField(max_length=20, verbose_name="Type")
+    MARKER = [('s', "Start"), ('f', "Finish")]
+    Marker = models.CharField(max_length=1, choices=MARKER)
 # Create your models here.
